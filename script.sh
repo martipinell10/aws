4f9d7fa46f223a03c4329f04383712710e53e74e
martipinell10 <martipime@outlook.es>
2024-02-17 18:51:41 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,48 +1,57 @@
 #!/bin/bash
 
-# Verificar si se ejecuta como superusuario
+# Verifica si som root o no
 if [ "$EUID" -ne 0 ]; then
-    echo "Por favor, ejecuta este script como superusuario."
+    echo "ERROR, l'script no s'executa com a usuari root."
     exit
 fi
 
-# Actualizar repositorios e instalar Apache
+
+# Actualitzar respositoris i instal·lar Apache2.
 apt-get update
 apt-get install -y apache2
 
-# Instalar PHP y módulos necesarios para WordPress
+
+# Instal·lar PHP i altres mòduls necesaris per a WordPress.
 apt-get install -y php libapache2-mod-php php-mysql
 
-# Descargar e instalar MySQL (MariaDB) y configurar contraseña root
+
+# Descarregar i instal·lar MySQL i configurar credencials de root.
 export DEBIAN_FRONTEND="noninteractive"
 debconf-set-selections <<< "mariadb-server-10.5 mysql-server/root_password password system"
 debconf-set-selections <<< "mariadb-server-10.5 mysql-server/root_password_again password system"
 apt-get install -y mariadb-server
 
-# Crear base de datos y usuario para WordPress
+# Crear Base de dades i usuari per a WordPress.
 mysql -u root -p"your_root_password" -e "CREATE DATABASE wordpress;"
 mysql -u root -p"your_root_password" -e "CREATE USER 'system'@'localhost' IDENTIFIED BY 'system';"
 mysql -u root -p"your_root_password" -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'system'@'localhost';"
 mysql -u root -p"your_root_password" -e "FLUSH PRIVILEGES;"
 
-# Descargar e instalar WordPress
+
+# Descarregar i instal·lar WordPress.
 cd /var/www/html
 wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 chown -R www-data:www-data /var/www/html/wordpress
 chmod -R 755 /var/www/html/wordpress
 
-# Configurar Apache para WordPress
+
+# Configurar Apache2 per a WordPress.
 cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/wordpress.conf
 echo "ServerName localhost" >> /etc/apache2/sites-available/wordpress.conf
 echo "DocumentRoot /var/www/html/wordpress" >> /etc/apache2/sites-available/wordpress.conf
 a2ensite wordpress.conf
 a2enmod rewrite
 
-# Reiniciar Apache
+
+# Reiniciar servei Apache2.
 systemctl restart apache2
 
-# Limpiar archivos temporales
+
+# Natejar fitxers temporals.
 rm -rf latest.tar.gz
 
-echo "Instalación completada. Accede a http://localhost/wordpress para configurar WordPress."
+
+# Comentari final.
+echo "Instal·lació completada. Siusplau, accedeixi a la pàgina de WordPress per configurar-lo.
